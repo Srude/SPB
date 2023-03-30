914f7f735255b09bdf337875a19b98c3eea1516a
오진서 <s22030@gsm.hs.kr>
2023-03-30 19:44:39 +0900

    GetMapping RequestParam

--- src/main/java/com/SPB/api/controller/GetController.java
+++ src/main/java/com/SPB/api/controller/GetController.java
@@ -1,14 +1,57 @@
 package com.SPB.api.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/get-api")
+@RequestMapping("/api")
 public class GetController {
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    public String getHello(){
-        return "Hello";
+    @GetMapping("/variable/{variable}")
+    public String getVariable(@PathVariable String variable){
+        return variable;
     }
+    //http://localhost:8088/api/variable/test
+
+    @GetMapping("/request1")
+    public String getRequestParam1(
+            @RequestParam String name, @RequestParam String email, @RequestParam String organization){
+
+        return name + " " + email + " " + organization;
+    }
+    //http://localhost:8088/api/request1?name=v&email=b&organization=n
+
+    @GetMapping("/request2")
+    public String getRequestParam2(@RequestParam Map<String,String> param){
+        StringBuilder builder = new StringBuilder();
+        param.entrySet().forEach(map->{
+            builder.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+        return builder.toString();
+    }
+    //http://localhost:8088/api/request2?key1=v&key2=b
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public class GetController {
+//    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+//    public String getHello(){
+//        return "Hello";
+//    }
+//}
